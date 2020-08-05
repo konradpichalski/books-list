4d014094d5e649467ab07b419d67a64c7c284c2b
konrad <konradpichalski@gmail.com>
2020-08-05 15:25:05 +0100

    Get current page from the browser search query, connect it to the state and pass some information down to TopBar component that displays the number of books and current page number

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+
+import api from '../../api/api';
 
 import Header from '../Header/Header';
 import TopBar from './TopBar';
 import BookInfo from './BookInfo';
 
-const BooksList = () => (
-  <Container>
-    <Header />
+const BooksList = () => {
+  // constants
+  const itemsPerPage = 20;
+  const location = useLocation();
+  const locationSearch = queryString.parse(location.search);
+  const { page } = locationSearch;
 
-    <TopBar />
+  // state
+  // convert the page to an integer for proper type
+  const [currentPage, setCurrentPage] = useState(parseInt(page) || 1);
+  const [books, setBooks] = useState([]);
+  const [booksCount, setBooksCount] = useState(0);
+  const [pageCount, setPageCout] = useState(1);
 
-    <Row>
-      <BookInfo />
-    </Row>
-  </Container>
-);
+  // useEffect calls
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataset = await api.fetchBooks({
+        page: currentPage,
+        itemsPerPage: 20,
+        filters: [],
+      });
+
+      const {
+        data: { books, count },
+      } = dataset;
+
+      setBooks(books);
+      setBooksCount(count);
+
+      // using Math.ceil to round to the next number so that last page is also visible
+      setPageCout(Math.ceil(count / itemsPerPage));
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <Header />
+
+      <TopBar
+        currentPage={currentPage}
+        pageCount={pageCount}
+        booksCount={booksCount}
+      />
+
+      <Row>
+        <BookInfo />
+      </Row>
+    </Container>
+  );
+};
 
 export default BooksList;
--- src/components/BooksList/TopBar.js
+++ src/components/BooksList/TopBar.js
@@ -1,23 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-const TopBar = () => (
+const TopBar = ({ currentPage, pageCount, booksCount }) => (
   <Row className="py-4">
     <Col xs={12} sm={3} lg={6} className="d-flex align-middle">
       <p className="h5 my-auto">
-        <b className="text-primary">100</b> books
+        <b className="text-primary">{booksCount}</b> books
       </p>
     </Col>
     <Col className="d-flex justify-content-end align-middle">
       <Button className="mr-3 my-3 flex-fill">Previous page</Button>
       <span className="text-secondary vertical-center my-auto mx-1">
-        Page <b>1</b> / <b>10</b>
+        Page <b>{currentPage}</b> / <b>{pageCount}</b>
       </span>
       <Button className="ml-3 my-3 flex-fill">Next page</Button>
     </Col>
   </Row>
 );
+
+TopBar.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  pageCount: PropTypes.number.isRequired,
+  booksCount: PropTypes.number.isRequired,
+};
 
 export default TopBar;
