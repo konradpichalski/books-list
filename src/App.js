9ee65f9888a323ab9a5ceaa6a6a77d0af21fadfd
konrad <konradpichalski@gmail.com>
2020-08-05 11:58:53 +0100

    Add react-bootstrap, update the theme primary colour and add a test display to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">Books list app</header>
-  </div>
+  <Container>
+    <Row>
+      <Col>
+        <h1 className="h1 primary">App</h1>
+        <Button variant="primary">This is a test</Button>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default App;
