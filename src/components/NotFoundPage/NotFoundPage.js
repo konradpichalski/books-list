5f0533d7a84b7d16954ba15214e15057188455b9
konrad <konradpichalski@gmail.com>
2020-08-06 11:42:03 +0100

    Update NotFoundPage component's button to handle redirect in the same way as the rest of the app and add NotFoundPage unit tests

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 import Header from '../Header/Header';
 
-const NotFoundPage = () => (
-  <Container>
-    <Header hideSearchForm={true} />
+const NotFoundPage = () => {
+  const history = useHistory();
 
-    <Card className="my-3" bg="light">
-      <Card.Body>
-        <Card.Title className="text-primary">Page not found</Card.Title>
+  const handleClick = () => history.push('/?page=1');
 
-        <Card.Text className="text-secondary mb-1">
-          <small className="text-muted">
-            Looks like something went wrong and we can't find the page you're
-            looking for. You can go back to main page by clicking the button.
-          </small>
-        </Card.Text>
+  return (
+    <Container>
+      <Header hideSearchForm={true} />
 
-        <Link to="/">
-          <Button variant="outline-primary my-3">Home page</Button>
-        </Link>
-      </Card.Body>
-    </Card>
-  </Container>
-);
+      <Card className="my-3" bg="light">
+        <Card.Body>
+          <Card.Title className="text-primary">Page not found</Card.Title>
+
+          <Card.Text className="text-secondary mb-1">
+            <small className="text-muted">
+              Looks like something went wrong and we can't find the page you're
+              looking for. You can go back to main page by clicking the button.
+            </small>
+          </Card.Text>
+
+          <Button variant="outline-primary my-3" onClick={handleClick}>
+            Home page
+          </Button>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
 
 export default NotFoundPage;
